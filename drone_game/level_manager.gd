ac7e3eb8579cc6db968ceb55fc6ccc09edb3e7bf
Noah MacMinn <plainoldnoah@me.com>
2022-08-01 12:28:00 -0400

    Added random spawn function for enemies

--- drone_game/level_manager.gd
+++ drone_game/level_manager.gd
@@ -3,19 +3,34 @@ extends Node2D
 var slug_path := preload("res://lifeforms/slug.tscn")
 
 func _ready():
-	spawn_enemy()
-	get_rand_pos()
+	rng.randomize()
 
-func spawn_enemy():
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		for i in 1000:
+			spawn_enemy(get_rand_pos())
+
+func spawn_enemy(pos:Vector2):
 	var enemy_scene:Node = slug_path.instance()
 	self.add_child(enemy_scene)
-	enemy_scene.global_position = Vector2(50, 50)
+	enemy_scene.init(pos)
 
 
 var rng:RandomNumberGenerator = RandomNumberGenerator.new()
 func get_rand_pos() -> Vector2:
 	var map_size:Vector2 = OS.window_size
 	
-	var generated:Vector2 = Vector2(rng.randi_range(0, 1024), rng.randi_range(0, 600))
-	print(map_size)
-	return Vector2.ZERO
+	var rand_num:int = rng.randi_range(1, 4)
+
+	match rand_num:
+		1: #Left
+			return(Vector2(0, rng.randi_range(0, map_size.y)))
+		2: #Right
+			return(Vector2(map_size.x, rng.randi_range(0, map_size.y)))
+		3: #Top
+			return(Vector2(rng.randi_range(0, map_size.x), 0))
+		4: #Bottom
+			return(Vector2(rng.randi_range(0, map_size.x), map_size.y))
+		_:
+			return Vector2.ZERO
--- drone_game/lifeforms/slug.gd
+++ drone_game/lifeforms/slug.gd
@@ -3,18 +3,24 @@ extends "res://lifeforms/generic_lifeform.gd"
 
 func _ready():
 	GroupMan.add_to_groups(self, ["SLUG", "ENEMY"])
+	yield(get_tree().create_timer(5), "timeout")
+	queue_free()
+
+
+func init(pos:Vector2):
+	global_position = pos
 	set_vel_to_hub()
-	
+
 
 # Different behavior for hitting drone or the hub
 func handle_collision(collision:KinematicCollision2D):
 	if collision.collider.is_in_group("HUB"):
 		queue_free()
-	elif collision.collider.is_in_group("DRONE"):
-		set_vel_to_hub()
-	else:
-		print("ERROR")
-
+#	elif collision.collider.is_in_group("DRONE"):
+#		set_vel_to_hub()
+#	else:
+#		pass
+#		print("ERROR")
 
 func set_vel_to_hub():
-	velocity = self.global_position.direction_to(Global.hub_scene.global_position) * speed
+	velocity = (Global.hub_scene.position - self.position).normalized() * speed
