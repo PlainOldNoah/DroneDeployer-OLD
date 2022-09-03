9da6fca2f214c3542a86d7081107541aa71c46db
Noah MacMinn <plainoldnoah@me.com>
2022-09-02 21:47:58 -0400

    Fabrication menu now has working crafting queue and cores

--- drone_game/core/globals.gd
+++ drone_game/core/globals.gd
@@ -5,3 +5,12 @@ var tile_size:int = 32
 var game_manager:Node = null
 var level_manager:Node = null
 var hub_scene:Node = null
+
+var crafting_options:Dictionary = {
+	"drone": {
+		"name":"Drone",
+		"scene":"res://lifeforms/drone.tscn",
+		"icon":"res://assets/small_drone.png",
+		"craft_time":3,
+	}
+}
