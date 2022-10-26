cdb1c6779ba88bb13dbf8bba57f1de8adfab6113
Noah MacMinn <plainoldnoah@me.com>
2022-10-26 16:44:05 -0400

    Preparing to merge into main

---
+++ drone_game/core/logger.gd
@@ -0,0 +1,22 @@
+extends Node
+
+signal message_created()
+
+var messages:PoolStringArray = []
+
+
+# Adds a new message to the logger
+func create(message:String):
+	messages.append(message)
+	emit_signal("message_created", message)
+	print_msg(message)
+
+
+# Removes all messages from the logger
+func clear():
+	messages = []
+
+
+# Prints out the message as well as what number message it is
+func print_msg(message:String):
+	print("LOGGER| ", messages.size(), ": ", message)
