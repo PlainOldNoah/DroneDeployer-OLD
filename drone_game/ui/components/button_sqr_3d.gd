0725040965fec8be7288fecd5ec7067cc1bc28e3
Noah MacMinn <plainoldnoah@me.com>
2023-03-16 17:56:05 -0400

    Finished square 3d button

--- drone_game/ui/components/button_sqr_3d.gd
+++ drone_game/ui/components/button_sqr_3d.gd
@@ -1,19 +1,43 @@
 extends Control
 
+signal pressed()
 
-@onready var test := $ButtonBase/TextureRect
 @onready var animation_player := $AnimationPlayer
+
+var mouse_hovering:bool = false
 
 
 func _ready():
 	animation_player.play("RESET")
 
 
+# Play the button down and then up animation, then check for mouse hover
 func play_animation():
-	animation_player.play("button_press")
+	animation_player.play("button_down")
+	emit_signal("pressed")
+	animation_player.queue("button_up")
 	await animation_player.animation_finished
-	animation_player.play_backwards("button_press")
+	
+	update_hover_texture()
 
 
-func _on_texture_button_pressed():
+# Show hovering texture if button is not being pressed and mouse hovering
+func update_hover_texture():
+	if mouse_hovering:
+		animation_player.queue("hover")
+	else:
+		animation_player.queue("RESET")
+
+
+func _on_click_region_pressed():
 	play_animation()
+
+
+func _on_click_region_mouse_entered():
+	mouse_hovering = true
+	update_hover_texture()
+
+
+func _on_click_region_mouse_exited():
+	mouse_hovering = false
+	update_hover_texture()
